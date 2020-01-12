9a477f89f65354a948ce111a57dfcab2ef9da2d8
bilgin.yuksel96 <bilginyuksel@users.noreply.github.com>
2020-01-13 01:10:04 +0300

    bad code written

--- GUI/calculator.py
+++ GUI/calculator.py
@@ -1,11 +1,11 @@
 import sys
 from PyQt5.QtWidgets import (QStyle, QWidget,QMainWindow,
 QApplication,QVBoxLayout,QGridLayout,QHBoxLayout,
-QPushButton,QLabel,QLineEdit,QTabWidget,QListWidget,QStackedLayout)
+QPushButton,QLabel,QLineEdit,QTabWidget,QListWidget,QMenuBar)
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QIcon,QColor, QPalette
+from PyQt5.QtGui import QIcon,QColor, QPalette, QFont
 from functools import partial
-
+from PyQt5 import QtGui,QtCore
 class CalculatorView(QWidget):
     
     def __init__(self, parent=None, flags=Qt.WindowFlags()):
@@ -20,6 +20,9 @@ class CalculatorView(QWidget):
         self.__set_input_layout()
         self.body_layout = QVBoxLayout()
         self.__set_body_layout()
+        label = QLabel("Scientific Calculator")
+        label.setFont(QFont("Times",13))
+        self.main_layout.addWidget(label)
         self.main_layout.addLayout(self.input_layout)
         self.main_layout.addLayout(self.body_layout)
         
@@ -30,51 +33,59 @@ class CalculatorView(QWidget):
         self.generalLayout.addLayout(self.memory_layout)
         self.setLayout(self.generalLayout)
 
-        self.is_functions_open = False
+        # Something is wrong with that
+        self.menu = QMenuBar (self)
+        self.calculator_type = self.menu.addMenu("Calculator")
+        self.calculator_help = self.menu.addMenu("Help")
+
+        
+        # self.is_functions_open = False
+        self.__op_functions()
 
     
     def __set_window_config(self):
         self.setWindowTitle("Calculator")
-        self.setWindowIcon(QIcon("calculator.png"))
-        self.setWindowOpacity(0.9)
+        self.setWindowIcon(QIcon("assets/calculator.png"))
+        self.setWindowOpacity(0.8)
+        self.setFont(QFont("Times",9))
         
 
     def __op_functions(self):
-        if self.is_functions_open:
-            # Delete all things inside functions
-            # self.is_functions_open = False
-            pass
-            # self.functions_layout.removeItem(self.functions_layout.itemAt(1))
-        # Add trigonometry functions here. 
-        # All add all functions here
-        # And when you press again, delete all functions.
-        else:
-            self.is_functions_open = True
-            # self.stack = QStackedLayout(self.input_layout)
+        # if self.is_functions_open:
+        #     # Delete all things inside functions
+        #     # self.is_functions_open = False
+        #     pass
+        #     # self.functions_layout.removeItem(self.functions_layout.itemAt(1))
+        # # Add trigonometry functions here. 
+        # # All add all functions here
+        # # And when you press again, delete all functions.
+        # else:
+        #     self.is_functions_open = True
+        #     # self.stack = QStackedLayout(self.input_layout)
 
-            h_layout = QHBoxLayout()
-            self.pu_sin = QPushButton("sin",self)
-            self.pu_cos = QPushButton("cos",self)
-            self.pu_tan = QPushButton("tan",self)
-            self.pu_cot = QPushButton("cot",self)
+        h_layout = QHBoxLayout()
+        self.pu_sin = QPushButton("sin",self)
+        self.pu_cos = QPushButton("cos",self)
+        self.pu_tan = QPushButton("tan",self)
+        self.pu_cot = QPushButton("cot",self)
 
-            h_layout.addWidget(self.pu_sin)
-            h_layout.addWidget(self.pu_cos)
-            h_layout.addWidget(self.pu_tan)
-            h_layout.addWidget(self.pu_cot)
-            h_layout.addStretch()
-            self.input_layout.addLayout(h_layout)
+        h_layout.addWidget(self.pu_sin)
+        h_layout.addWidget(self.pu_cos)
+        h_layout.addWidget(self.pu_tan)
+        h_layout.addWidget(self.pu_cot)
+        h_layout.addStretch()
+        self.input_layout.addLayout(h_layout)
 
-            h_layout = QHBoxLayout()
-            self.pu_cosec = QPushButton("csc",self)
-            self.pu_sec = QPushButton("sec",self)
-            self.pu_rand = QPushButton("rand",self)
+        h_layout = QHBoxLayout()
+        self.pu_cosec = QPushButton("csc",self)
+        self.pu_sec = QPushButton("sec",self)
+        self.pu_rand = QPushButton("rand",self)
 
-            h_layout.addWidget(self.pu_cosec)
-            h_layout.addWidget(self.pu_sec)
-            h_layout.addWidget(self.pu_rand)
-            h_layout.addStretch()
-            self.input_layout.addLayout(h_layout)
+        h_layout.addWidget(self.pu_cosec)
+        h_layout.addWidget(self.pu_sec)
+        h_layout.addWidget(self.pu_rand)
+        h_layout.addStretch()
+        self.input_layout.addLayout(h_layout)
 
         
 
@@ -82,15 +93,17 @@ class CalculatorView(QWidget):
         self.line_input = QLineEdit("0")
         self.line_input.setReadOnly(True)
         self.line_input.setAlignment(Qt.AlignRight)
+        self.line_input.setFont(QFont("Consolas",13,QFont.Bold))
         self.input_layout.addWidget(self.line_input)
 
         # Always update the current operation here.
         h_box = QHBoxLayout()
-        self.functions = QPushButton("Functions",self)
-        self.functions.clicked.connect(self.__op_functions)
-        h_box.addWidget(self.functions)
+        # self.functions = QPushButton("Functions",self)
+        # self.functions.clicked.connect(self.__op_functions)
+        # h_box.addWidget(self.functions)
         self.ghost_compute = QLabel('0')
         self.ghost_compute.setAlignment(Qt.AlignRight)
+        self.ghost_compute.setFont(QFont("Consolas",13))
         h_box.addWidget(self.ghost_compute)
         self.input_layout.addLayout(h_box)
 
@@ -186,12 +199,14 @@ class CalculatorView(QWidget):
         hbox.addWidget(self.pu_equal)
         self.body_layout.addLayout(hbox)
 
+
     def __set_memory_layout(self):
         self.memory_layout_tabs = QTabWidget()
         self.list_memory_history = QListWidget()
         self.list_memory_history.addItem("No element on history.")
         self.list_memory_formulas = QListWidget()
         self.list_memory_formulas.addItem("No element on formulas.")
+
         self.memory_layout_tabs.addTab(self.list_memory_history,"History")
         self.memory_layout_tabs.addTab(self.list_memory_formulas,"Formulas")
         self.memory_layout.addWidget(self.memory_layout_tabs)
@@ -221,6 +236,7 @@ class CalculatorController:
         self._model = model
         self._view = view
         
+        self.token = ""
         self.display = ""
         self.current_number = ""
         self.open_paranthesis = 0
@@ -243,33 +259,79 @@ class CalculatorController:
         self._view.pu_paranthesis_start.clicked.connect(self.pb_paranthesis_start)
         self._view.pu_paranthesis_end.clicked.connect(self.pb_paranthesis_end)
 
-        self._view.pu_add.clicked.connect(self.pb_add)
-        self._view.pu_subtract.clicked.connect(self.pb_subtract)
-        self._view.pu_divide.clicked.connect(self.pb_divide)
-        self._view.pu_multip.clicked.connect(self.pb_multip)
+        self._view.pu_add.clicked.connect(partial(self.pb_operator,"+"))
+        self._view.pu_subtract.clicked.connect(partial(self.pb_operator,"-"))
+        self._view.pu_divide.clicked.connect(partial(self.pb_operator,"/"))
+        self._view.pu_multip.clicked.connect(partial(self.pb_operator,"*"))
 
         self._view.pu_negative.clicked.connect(self.pb_negative)
+        self._view.pu_equal.clicked.connect(self.pb_equal)
 
-    def pb_add(self):
-        if self.current_number != "":
-            self.display += "+"
-            self.current_number =""
+        self._view.pu_pi.clicked.connect(partial(self.pb_constants,'pi'))
+        self._view.pu_e.clicked.connect(partial(self.pb_constants,'e'))
+        self._view.pu_clear.clicked.connect(self.pb_clear)
+
+        self._view.pu_poweroftwo.clicked.connect(partial(self.func_parameter1,'2^x'))
+        self._view.pu_xsquare.clicked.connect(partial(self.func_parameter1,'square(x)'))
+        self._view.pu_onedividex.clicked.connect(partial(self.func_parameter1,'1/x'))
+        self._view.pu_abs.clicked.connect(partial(self.func_parameter1,'abs(x)'))
+        self._view.pu_exp.clicked.connect(partial(self.func_parameter1,"exp(x)"))
+        self._view.pu_mod.clicked.connect(self.pb_mod)
+        self._view.pu_xcube.clicked.connect(partial(self.func_parameter1,'cube(x)'))
+        self._view.pu_factorial.clicked.connect(partial(self.func_parameter1,"fact(x)"))
+        self._view.pu_xpower.clicked.connect(self.pb_xpower)
+        self._view.pu_tentopowerx.clicked.connect(partial(self.func_parameter1,'10^x'))
+        self._view.pu_log.clicked.connect(partial(self.func_parameter1,"log"))
+        self._view.pu_ln.clicked.connect(partial(self.func_parameter1,"ln(x)"))
+
+        self._view.pu_sin.clicked.connect(partial(self.pb_trigonometry_functions,"sin"))
+        self._view.pu_cos.clicked.connect(partial(self.pb_trigonometry_functions,"cos"))
+        self._view.pu_tan.clicked.connect(partial(self.pb_trigonometry_functions,"tan"))
+        self._view.pu_cot.clicked.connect(partial(self.pb_trigonometry_functions,"cot"))
+        self._view.pu_cosec.clicked.connect(partial(self.pb_trigonometry_functions,"cosec"))
+        self._view.pu_sec.clicked.connect(partial(self.pb_trigonometry_functions,"sec"))        
+        # it is not trigonometric but...
+        self._view.pu_rand.clicked.connect(partial(self.pb_trigonometry_functions,"rand"))
+
+
+    def pb_constants(self,const):
+        if len(self.display)>0 and (self.display[-1]==")" or self.__isfloat(self.current_number)):
+            pass
+        else:
+            if const == "pi":   
+                self.current_number += str(22/7)
+            elif const == 'e':
+                self.current_number += str((1+(1/10000))**10000)
+                print(self.current_number)
+
+            self.display += const
             self._view.set_display_line_edit(self.display)
-    def pb_subtract(self):
+            self._view.set_display_label(const)
+
+    def pb_trigonometry_functions(self,func_name):
+        pass
+
+    def func_parameter1(self,param1):
+        pass
+
+
+    def pb_log(self):
+        pass
+
+
+    def pb_mod(self):
+        pass
+
+    def pb_xpower(self):
+        pass
+
+    def pb_operator(self,op):
         if self.current_number != "":
-            self.display += "-"
+            self.display += op
+            self.token += self.current_number + "|"+op+"|"
             self.current_number = ""
             self._view.set_display_line_edit(self.display)
-    def pb_divide(self):
-        if self.current_number != "":
-            self.display += "/"
-            self.current_number = ""
-            self._view.set_display_line_edit(self.display)
-    def pb_multip(self):
-        if self.current_number != "":
-            self.display += "*"
-            self.current_number = ""
-            self._view.set_display_line_edit(self.display)
+
     def pb_negative(self):
         if self.__isfloat(self.current_number):
             n_disp = self.display[:len(self.display)-len(self.current_number)]
@@ -279,7 +341,7 @@ class CalculatorController:
             self._view.set_display_line_edit(self.display)   
     def pb_numbers(self,obj):
         # Don't allow paranthesis
-        if len(self.display)>0 and self.display[-1]==")":
+        if len(self.display)>0 and (self.display[-1]==")" or self.display[-1]=='e' or self.display[-1]=='i') :
             pass
         else:
             self.display += obj
@@ -289,11 +351,16 @@ class CalculatorController:
     def pb_backspace(self):
         if len(self.display)<=1:
             self.display =""
+            self.token = ""
             self.current_number=""
             self._view.set_display_line_edit("0")
         else: 
             self.display = self.display[:len(self.display)-1]
             self.current_number = self.current_number[:len(self.current_number)-1]
+
+            if self.token[-1] == '|': self.token = self.token[:len(self.token)-2]
+            else: self.token = self.token[:len(self.token)-1]
+
             print(self.current_number)
             self._view.set_display_line_edit(self.display)
         self._view.set_display_label("0")  
@@ -306,14 +373,31 @@ class CalculatorController:
     def pb_paranthesis_start(self):
         if self.current_number=="":
             self.display += "("
+            self.token += "(|"
             self.open_paranthesis+=1
             self._view.set_display_line_edit(self.display)
     def pb_paranthesis_end(self):
         if self.__isfloat(self.current_number) and self.open_paranthesis>0:
             self.open_paranthesis -= 1
             self.display += ")"
+            self.token += self.current_number+"|)|"
             self._view.set_display_line_edit(self.display)
             self.current_number = ")"
+    def pb_equal(self):
+        self.token = (self.token + self.current_number) if self.__isfloat(self.current_number) else self.token[:len(self.token)-1]
+        print(self.token)
+        print(self.token.split("|"))
+
+
+    
+
+    def pb_clear(self):
+        self.open_paranthesis = 0
+        self.display = ""
+        self.token = ""
+        self.current_number = ""
+        self._view.set_display_line_edit("0")
+        self._view.set_display_label("0")
 
     def __isfloat(self,num):
         try:
@@ -322,6 +406,11 @@ class CalculatorController:
         except ValueError:
             print("Value error")
             return False
+
+
+
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
