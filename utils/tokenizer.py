a08c71b16ca8c79bd0f2a71a23cc4a7af695518c
bilgin.yuksel96 <bilginyuksel@users.noreply.github.com>
2020-01-17 02:40:35 +0300

    yuksel updated

--- utils/tokenizer.py
+++ utils/tokenizer.py
@@ -1,10 +1,50 @@
-def vector1d(content):
-    # Function properties
-    return content
+try:
+    import sys
+    sys.path.append("..")
+    
+    from utils.component import *
+except ImportError as e:
+    print(e)
+
+from calculator.functions.realfunctions import RealFunctions
+from calculator.real import Real
 def real(content):
-    # Function properties
-    # shutting yard algorithm
-    return content
+    valStack = []
+    opStack = []
+    for item in content:
+        if(isinstance(item,ComponentValue)):
+            valStack.append(item.val)
+        elif(isinstance(item,ComponentFunction)):
+            funcVal = real(item.expression)
+            rf = RealFunctions(item.func,funcVal)
+            valStack.append(rf.solve)
+        elif(isinstance(item,ComponentParanthesis)):
+            if(item.paranthesis== '('):
+                opStack.append(item.paranthesis)
+            else:
+                while opStack[-1]!='(':
+                    op = opStack.pop()
+                    rightVal = valStack.pop()
+                    leftVal = valStack.pop()
+                    r = Real(op)
+                    valStack.append(r.solve(leftVal,rightVal))
+                opStack.pop()
+        elif(isinstance(item,ComponentOperator)):
+            while len(opStack)>0 and ComponentOperator(opStack[-1]).precedence>=item.precedence:
+                op = opStack.pop()
+                rightVal = valStack.pop()
+                leftVal = valStack.pop()
+                r = Real(op)
+                valStack.append(r.solve(leftVal,rightVal))
+            opStack.append(item.op)
+        
+    while len(opStack)>0:
+        op = opStack.pop()
+        rightVal = valStack.pop()
+        leftVal = valStack.pop()
+        r = Real(op)
+        valStack.append(r.solve(leftVal,rightVal))
+    return valStack.pop()
 
 class Tokenizer:
     
@@ -13,7 +53,6 @@ class Tokenizer:
         self.tokenizer_type = tType
 
         self.tokenizers = {
-            "vector1d":vector1d,
             "real":real
         }
 
@@ -22,8 +61,8 @@ class Tokenizer:
         return self.tokenizers[self.tokenizer_type](content)
 
 
-vectorTokenizer = Tokenizer("vector1d")
-result = vectorTokenizer.tokenize("[1,2] + [3,4]/2")
+# vectorTokenizer = Tokenizer("vector1d")
+# result = vectorTokenizer.tokenize("[1,2] + [3,4]/2")
 
-complexTokenizer = Tokenizer("real")
-result = complexTokenizer.tokenize("sin(100)/2 + (5*cos(180))")
+# complexTokenizer = Tokenizer("real")
+# result = complexTokenizer.tokenize("sin(100)/2 + (5*cos(180))")
